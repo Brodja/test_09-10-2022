8eb737598cd3cc53eb85ccf3e69e39714390eee4
Brodja <body1906@gmail.com>
2022-10-09 20:02:04 +0300

    aded code

--- app/controllers/balance.controller.js
+++ app/controllers/balance.controller.js
@@ -5,26 +5,49 @@ const mongoose = require("mongoose");
 const db = require("../config/db");
 
 exports.findOne = async (req, res) => {
-  const my_id = req.params.id;
+  const my_id = +req.params.id;
   const currency = req.params.currency;
-  const user = await User.findOne({ my_id: +my_id });
+  if (
+    typeof my_id !== "number" ||
+    my_id < 1 ||
+    (currency && typeof currency !== "string")
+  ) {
+    res.status(400).json("params error");
+    return;
+  }
+  const user = await User.findOne({ my_id: my_id });
 
   if (user) {
     if (currency) {
       const balance_convert = await getCurr(currency, user.balance);
-      res.json({ balance: balance_convert.result });
+      console.log(balance_convert);
+      if (!balance_convert || !balance_convert.success) {
+        res.status(500).json({ message: "error" });
+      } else {
+        res.json({ balance: balance_convert.result });
+      }
     } else {
       res.json({ balance: user.balance });
     }
   } else {
-    res.json({ message: "User not found" });
+    res.status(404).json({ message: "User not found" });
   }
 };
 
 exports.change = async (req, res) => {
   const { id, value, income } = req.body;
+  if (
+    typeof value !== "number" ||
+    value < 0 ||
+    typeof income !== "boolean" ||
+    typeof id !== "number" ||
+    id < 1
+  ) {
+    res.status(400).json("params error");
+    return;
+  }
   try {
-    const user = await User.findOne({ my_id: +id });
+    const user = await User.findOne({ my_id: id });
     if (!user) {
       throw new Error("User not found!");
     } else {
@@ -32,29 +55,41 @@ exports.change = async (req, res) => {
         throw new Error("Insufficient funds!");
       } else {
         const newBalance = income ? user.balance + value : user.balance - value;
-        await User.findOneAndUpdate({ my_id: +id }, { balance: newBalance });
-       
+        await User.findOneAndUpdate({ my_id: id }, { balance: newBalance });
+
         const Log = {
           id_from: !income ? user.my_id : "",
           id_to: income ? user.my_id : "",
           value,
-          description: "Simple operation"
-        }
+          description: "Simple operation",
+        };
         await Story.create(Log);
       }
     }
     res.json("success");
   } catch (error) {
     console.log(error);
-    res.json(error.message);
+    res.status(404).json(error.message);
   }
 };
 
 exports.transfer = async (req, res) => {
   const { id_from, id_to, value, description } = req.body;
+  if (
+    typeof value !== "number" ||
+    value < 0 ||
+    typeof income !== "boolean" ||
+    typeof id_from !== "number" ||
+    id_from < 1 ||
+    typeof id_to !== "number" ||
+    id_to < 1
+  ) {
+    res.status(400).json("params error");
+    return;
+  }
   try {
-    const user_from = await User.findOne({ my_id: +id_from });
-    const user_to = await User.findOne({ my_id: +id_to });
+    const user_from = await User.findOne({ my_id: id_from });
+    const user_to = await User.findOne({ my_id: id_to });
     if (!user_from && !user_to) {
       throw new Error("User not found!");
     } else {
@@ -64,53 +99,49 @@ exports.transfer = async (req, res) => {
         const newBalanceFrom = user_from.balance - value;
         const newBalanceTo = user_to.balance + value;
         await User.findOneAndUpdate(
-          { my_id: +id_from },
+          { my_id: id_from },
           { balance: newBalanceFrom }
         );
         await User.findOneAndUpdate(
-          { my_id: +id_to },
+          { my_id: id_to },
           { balance: newBalanceTo }
         );
         const Log = {
           id_from: user_from.my_id,
           id_to: user_to.my_id,
           value,
-          description
-        }
+          description,
+        };
         await Story.create(Log);
       }
     }
     res.json("success");
   } catch (error) {
     console.log(error);
-    res.json(error.message);
+    res.status(404).json(error.message);
   }
 };
 
 async function getCurr(to, amount) {
-  const myHeaders = new Headers();
-  myHeaders.append("apikey", process.env.CurrApiKey);
-
-  const requestOptions = {
-    method: "GET",
-    redirect: "follow",
-    headers: myHeaders,
-  };
-
-  const from = "USD";
-
-  const response = await fetch(
-    `https://api.apilayer.com/currency_data/convert?to=${to}&from=${from}&amount=${amount}`,
-    requestOptions
-  );
-  const result = await response.text();
-
-  return JSON.parse(result);
-  // .then((response) => response.text())
-  // .then((result) => {
-  //   return result;
-  // })
-  // .catch((error) => console.log("error", error));
+  try {
+    const myHeaders = new Headers();
+    myHeaders.append("apikey", process.env.CurrApiKey);
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+      headers: myHeaders,
+    };
+    const from = "USD";
+    const response = await fetch(
+      `https://api.apilayer.com/currency_data/convert?to=${to}&from=${from}&amount=${amount}`,
+      requestOptions
+    );
+    const result = await response.text();
+    return JSON.parse(result);
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 
 // async function operation(id, value, type){
